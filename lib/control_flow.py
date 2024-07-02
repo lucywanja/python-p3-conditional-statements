a9ccb35ea6069876d15d25ce86bc777cf74334d1
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-07-02 21:25:02 +0300

    Added the solutions

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,69 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username.lower() == "admin" and password == "12345":
+        return "Access granted"
+
+    return "Access denied" 
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"  
+
+    return "It's perfect out there!"      
+    
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 15 == 0:
+        return "FizzBuzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    
+    return num
+    
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+
+    print("Invalid operation!")
+    return None  
+
+
+
+# print(admin_login("sudo", "12345"))  # "Access denied"
+# print(admin_login("admin", "12345"))  # "Access granted"
+# print(admin_login("ADMIN", "12345"))  # "Access granted"
+
+# print(hows_the_weather(33))  # "It's brisk out there!"
+# print(hows_the_weather(99))  # "It's too dang hot out there!"
+# print(hows_the_weather(75))  # "It's perfect out there!"
+
+# print(fizzbuzz(1))  # 1
+# print(fizzbuzz(2))  # 2
+# print(fizzbuzz(3))  # "Fizz"
+# print(fizzbuzz(4))  # 4
+# print(fizzbuzz(5))  # "Buzz"
+# print(fizzbuzz(15))  # "FizzBuzz"
+
+# print(calculator("+", 1, 1))  # 2
+# print(calculator("-", 3, 1))  # 2
+# print(calculator("*", 3, 2))  # 6
+# print(calculator("/", 4, 2))  # 2.0
+# print(calculator("nope", 4, 2))  # "Invalid operation!" None      
+
+    
+    
